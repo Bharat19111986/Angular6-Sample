b7b24b469c3b7b677288e41424ff4b4c3f87a488
Bharat Bhaskar <bharat.bhaskar@cdk.com>
2019-02-20 19:36:43 +0530

    Angular6-Seed
    Angular6-Seed

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RootModule, UIRouterModule } from '@uirouter/angular';
+
+const rootModule: RootModule = {
+    states: [],
+    useHash: true
+};
+
+@NgModule({
+    imports: [UIRouterModule.forRoot(rootModule)],
+    exports: [UIRouterModule]
+})
+export class AppRoutingModule { }
---
+++ webpack.config.dev.js
@@ -0,0 +1,33 @@
+var path = require('path');
+
+var webpackMerge = require('webpack-merge');
+var commonConfig = require('./webpack.config.common');
+var valPath = path.resolve(__dirname,'dist');
+console.log('File Path present',valPath);
+module.exports = webpackMerge(commonConfig,{
+    output:{
+        path:path.resolve(__dirname,'dist'),
+        publicPath: '/',
+        filename:'bundle.js',
+        chunkFilename:'[id].chunk.js'
+    },
+    mode:'development',
+    module:{
+      rules:[
+          {
+              test:/\.ts$/,
+              use:[
+                  {loader:'awesome-typescript-loader',options:{
+                      transpileOnly:true
+                      }},
+                  {loader:'angular2-template-loader'},
+                  {loader:'angular-router-loader'}
+              ]
+          }
+      ]
+    },
+    devServer:{
+        historyApiFallback:true,
+        stats:'minimal'
+    }
+})
