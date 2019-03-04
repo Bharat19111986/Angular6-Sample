23e07235b1a189e6b96c1a544447b17c86dff259
Bharat Bhaskar <bharat.bhaskar@cdk.com>
2019-03-04 17:57:58 +0530

    Less Compilation step

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,11 +1,25 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
 var path = require('path');
 var webpack = require('webpack');
+let pathsToClean = [
+    'dist',
+    'build'
+]
+
+// the clean options to use
+let cleanOptions = {
+    root:     '/full/webpack/root/path',
+    exclude:  ['shared.js'],
+    verbose:  true,
+    dry:      false
+}
 module.exports={
     entry: './main.ts',
     resolve:{
         extensions: ['.js','.ts']
     },
+
     module: {
         rules: [
             {
@@ -13,14 +27,38 @@ module.exports={
                 loaders:['html-loader']
             },
             {
+                test: /\.less$/,
+                use: [{
+                    loader: 'raw-loader'
+                }, {
+                    loader: 'less-loader', options: {
+                        strictMath: true,
+                        noIeCompat: true
+                    }
+                }]
+            },
+            {
                 test: /\.css$/,
                 loaders:['raw-loader']
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/, loader: 'file?name=[name].[ext]'
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use:[{
+                    loader: 'file-loader'
+                }]
             }
         ],
+
         exprContextCritical: false
     },
 
     plugins: [
+        new CleanWebpackPlugin(
+            pathsToClean, cleanOptions
+        ),
         new HtmlWebpackPlugin({
             template:'index.html'
         })
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,6 +5,7 @@ var commonConfig = require('./webpack.config.common');
 var valPath = path.resolve(__dirname,'dist');
 console.log('File Path present',valPath);
 module.exports = webpackMerge(commonConfig,{
+    devtool: 'inline-source-map',
     output:{
         path:path.resolve(__dirname,'dist'),
         publicPath: '/',
